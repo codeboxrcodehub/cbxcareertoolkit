dfd347cb392c28abc74ff351500c015ebdb06c80
Deepak3642 <deepakdeb001@gmail.com>
2025-01-26 15:09:59 +0600

    command issue fixed

--- includes/Factories/Misc/EasyPluginChecker.php
+++ includes/Factories/Misc/EasyPluginChecker.php
@@ -1,12 +1,13 @@
 <?php
 namespace Cbx\Careertoolkit\Factories\Misc;
+use Cbx\Careertoolkit\Factories\Factory;
 
 /**
  * Easy plugin checker command command
  * Class EasyPluginChecker
  * @since 1.0.1
  */
-class EasyPluginChecker{
+class EasyPluginChecker extends Factory{
 
 	/**
 	 * Register CLI command
@@ -25,12 +26,13 @@ class EasyPluginChecker{
 	 * @since 1.0.0
 	 */
 	public function run( $args, $assoc_args ) {
+		\WP_CLI::success("Plugin check output written to");
 		global $wpdb;
 
 		$plugin = $args[0];
-		$exclude_directories = WP_CLI\Utils\get_flag_value($assoc_args, 'exclude-directories', '');
-		$format = WP_CLI\Utils\get_flag_value($assoc_args, 'format', 'table'); // Default format is table
-		$output_file = WP_CLI\Utils\get_flag_value($assoc_args, 'output-file', '');
+		$exclude_directories = \WP_CLI\Utils\get_flag_value($assoc_args, 'exclude-directories', '');
+		$format = \WP_CLI\Utils\get_flag_value($assoc_args, 'format', 'table'); // Default format is table
+		$output_file = \WP_CLI\Utils\get_flag_value($assoc_args, 'output-file', '');
 
 		// Set default output file if not provided
 		if (empty($output_file)) {
@@ -49,7 +51,7 @@ class EasyPluginChecker{
 		exec($command, $output, $return_var);
 
 		if ($return_var !== 0) {
-			WP_CLI::error('Error running the plugin check command.');
+			\WP_CLI::error('Error running the plugin check command.');
 		}
 
 		// Combine the output into a single string
@@ -57,14 +59,14 @@ class EasyPluginChecker{
 
 		// Clean the file (truncate) before writing new output
 		if (file_put_contents($output_file, '') === false) {
-			WP_CLI::error('Failed to clear the output file.');
+			\WP_CLI::error('Failed to clear the output file.');
 		}
 
 		// Write the output to the file
 		if (file_put_contents($output_file, $output_content) === false) {
-			WP_CLI::error('Failed to write output to the file.');
+			\WP_CLI::error('Failed to write output to the file.');
 		}
 
-		WP_CLI::success("Plugin check output written to {$output_file}");
+		\WP_CLI::success("Plugin check output written to {$output_file}");
 	} //end method run
 } //end class EasyPluginChecker
